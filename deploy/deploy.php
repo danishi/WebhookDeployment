54c60ae9d23d2fa2486d3b78f7e6609c9eadbf32
danishi <mail@example.com>
2019-05-20 22:23:11 +0900

    修正

--- deploy/deploy.php
+++ deploy/deploy.php
@@ -1,11 +1,13 @@
 <?php
+$log_file = __DIR__ . "/../depoy.log";
+
 // INIファイル読み込み
-$arrayIniFile = parse_ini_file($_SERVER['DOCUMENT_ROOT'] . "../deploy.ini", true);
+$arrayIniFile = parse_ini_file(__DIR__ . "/../deploy.ini", true);
 
 // secretキーのチェック
 if(!isset($_SERVER['HTTP_X_HUB_SIGNATURE']) &&
    !($_SERVER['HTTP_X_HUB_SIGNATURE'] === sha1($arrayIniFile['sercret_key']))){
-    file_put_contents('depoy.log', date("Y-m-d H:i:s")."\t".'secret key error', FILE_APPEND);
+    file_put_contents($log_file, date("Y-m-d H:i:s")."\t".'secret key error', FILE_APPEND);
     die();
 }
 
@@ -17,10 +19,10 @@ foreach($arrayIniFile['deploy_setting'] as $repository_name => $local_repository
 
     $command = 'cd ' . $local_repository_path;
     shell_exec($command, $output, $return_var);
-    file_put_contents('depoy.log', date("Y-m-d H:i:s")."\t".$command."\t".$output, FILE_APPEND);
+    file_put_contents($log_file, date("Y-m-d H:i:s")."\t".$command."\t".$output, FILE_APPEND);
     if(!$return_var) continue;
 
     $command = 'git --git-dir=.git pull';
-    file_put_contents('depoy.log', date("Y-m-d H:i:s")."\t".$command."\t".$output, FILE_APPEND);
+    file_put_contents($log_file, date("Y-m-d H:i:s")."\t".$command."\t".$output, FILE_APPEND);
     shell_exec($command, $output, $return_var);
 }
